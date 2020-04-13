5a5faac60f4934007f2a23e79761fd563f40333a
WinIT23 <chauhanvinit23@gmail.com>
2020-04-13 14:11:50 +0530

    Model Classes updated

--- src/main/java/com/adit/oep/model/Student.java
+++ src/main/java/com/adit/oep/model/Student.java
@@ -5,6 +5,8 @@
  */
 package com.adit.oep.model;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -16,42 +18,70 @@ import javax.persistence.Transient;
  *
  * @author Win_It
  */
-
 @Entity
-@Table(name="student_login")
-public class Student {
-    @Column(name="student_name")
+@Table(name = "student_login")
+public class Student implements Serializable {
+
+    @Column(name = "student_name")
     private String sName;
     @Id
-    @Column(name="student_id")
+    @Column(name = "student_id")
     private long sEnNumber;
-    @Column(name="student_pass")
+    @Column(name = "student_pass")
     private String sPassword;
     @Transient
     private int sBranchCode;
     @Transient
     private int sEntryYear;
-    
+
     @Transient
     private List<Assignment> assignmentDone;
     @Transient
     private List<Assignment> assignmentRemain;
 
-    public Student()
-    {
+    public Student() {
         this.sEnNumber = 0L;
     }
-    public Student(long sEnNumber,String sPassword)
-    {
+
+    public Student(long sEnNumber, String sPassword) {
         this.sEnNumber = sEnNumber;
         this.sPassword = sPassword;
     }
+
     public Student(String sName, long sEnNumber, String sPassword) {
         this.sName = sName;
         this.sEnNumber = sEnNumber;
         this.sPassword = sPassword;
 
         processEnrollmentNumber();
+    }
+
+    public String getsPassword() {
+        return sPassword;
+    }
+
+    public long getsEnNumber() {
+        return sEnNumber;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsPassword(String sPassword) {
+        this.sPassword = sPassword;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public void setsEnNumber(long sEnNumber) {
+        this.sEnNumber = sEnNumber;
+    }
+
+    public void setSEnNumber(long sEnNumber) {
+        this.sEnNumber = sEnNumber;
     }
 
     private void processEnrollmentNumber() {
@@ -62,40 +92,22 @@ public class Student {
 
         this.sEntryYear = 2000 + (int) temp;
     }
-    
-    public String getsPassword()
-    {
-        return sPassword;
-    }
-    
-    public long getsEnNumber()
-    {
-        return sEnNumber;
-    }
-    
-    public String getsName()
-    {
-        return sName;
-    }
-    
-    public void setsPassword(String sPassword)
-    {
-        this.sPassword = sPassword;
-    }
-    
-    public void setsName(String sName)
-    {
-        this.sName = sName;
-    }
-    
-    public void setsEnNumber(long sEnNumber)
-    {
-        this.sEnNumber = sEnNumber;
-    }
 
+    private void refreshList() {
+        for (Assignment as : this.assignmentRemain) {
+            if (as.isComplete()) {
+                this.assignmentRemain.remove(as);
+                this.assignmentDone.add(as);
+            }
+        }
+    }
     
-
-    public void setSEnNumber(long sEnNumber) {
-        this.sEnNumber = sEnNumber;
+    public void addAssignment(String aName, int subCode, String todayDate, String subDates, String facultyName) {
+        
+        // TO-DO :  Get subject name from a predefined list
+        this.assignmentRemain.add(new Assignment(aName,
+                                                new Subject(subCode, 
+                                                            facultyName), 
+                                                todayDate, todayDate));
     }
 }
--- src/main/java/com/adit/oep/model/Subject.java
+++ src/main/java/com/adit/oep/model/Subject.java
@@ -5,30 +5,34 @@
  */
 package com.adit.oep.model;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Win_It
  */
 public class Subject {
-    
+
+    private static final HashMap<Integer, String> SUBJECT_MAP = new HashMap<Integer, String>();
+
     private final String subName;
     private final int subCode;
     private final String faculty;
-    
-    public Subject(String subName, int subCode, String faculty) {
-        this.subName = subName;
+
+    public Subject(int subCode, String faculty) {
+        this.subName = SUBJECT_MAP.get(subCode) != null ? SUBJECT_MAP.get(subCode) : "";
         this.subCode = subCode;
         this.faculty = faculty;
     }
-    
+
     public String getSubName() {
         return this.subName;
     }
-    
+
     public int getSubCode() {
         return this.subCode;
     }
-    
+
     public String getFaculty() {
         return this.faculty;
     }
